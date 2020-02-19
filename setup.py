e7537bb87ba6277851dcf8094a1bf58eef134c0f
Louis Lefevre <LouisLefev@gmail.com>
2020-02-19 21:56:54 +0000

    Remove __version__.py
    Removed the version file so that the version is instead defined directly
    in the setup.py file.

--- setup.py
+++ setup.py
@@ -21,18 +21,9 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
